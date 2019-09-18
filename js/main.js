b7c69930671d64f012fdea4a16a7ec2dd5021717
yakushiji <m-yakushiji@haiming.co.jp>
2019-09-18 12:25:50 +0900

    'update'

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-function test() {
-    document.getElementById("upload_chat").click();
+function test(target) {
+    document.getElementById(target).click();
     return false;
+}
+
+function test2(event, target) {
+    var file = event.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function() {
+        document.getElementById(target).src = reader.result;
+    }
+}
+
+function test3(target) {
+    document.getElementById(target).value = "test";
 }
